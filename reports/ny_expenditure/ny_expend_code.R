3dfc267c359055faa0fd5a0cfd7d2a003936dba9
Andrew Bates <andrewbates73@gmail.com>
2018-09-30 15:05:37 -0700

    add some simple eda. specifically summary statistics, correlation plot, scatterplot matrix

--- reports/ny_expenditure/ny_expend_code.R
+++ reports/ny_expenditure/ny_expend_code.R
@@ -1,6 +1,7 @@
 
 library(readr)
 library(dplyr)
+library(corrplot)
 
 ny <- read_csv("reports/ny_expenditure/ny_expend.csv")
 
@@ -30,4 +31,26 @@ ny <- ny %>%
          grow_rate)
 
 write_csv(ny, "reports/ny_expenditure/ny_expend.csv")
+# =======================================================
+
+# =======================================================
+# ----------------- EDA ------------------------
+summary(ny)
+ny <- filter(ny, !is.na(expenditure))
+dim(ny) # 914 x 7
+summary(ny)
+corr_mat <- cor(ny)
+corrplot(corr_mat, type = "upper", diag = FALSE) # correlation matrix plot
+pairs(ny) # scatterplot matrix
+# see also argument log to pairs. and diag.panel 
+# also look at 580 code to see how to add density plot
+
+
+
+
+
+
+
+
+
 # =======================================================
