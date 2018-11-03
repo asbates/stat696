295b3710d78dbaf3d293be5d89da9b2828e364fc
Andrew Bates <andrewbates73@gmail.com>
2018-11-03 16:53:54 -0700

    start intro. add variable description table

--- reports/prostate-cancer/wip-code.R
+++ reports/prostate-cancer/wip-code.R
@@ -5,4 +5,19 @@ library(dplyr)
 
 prostate <- read_tsv(here("data", "prostate.txt"))
 
+# ======================
+# ======= EDA ==========
+# ======================
+
+dim(prostate)
+
+# how many have experience capsule penetration?
+prostate %>%
+  group_by(capsule) %>%
+  summarise(n = n())
+
+# any missing values?
+apply(prostate, 2, function(x)sum(is.na(x)))
+
+
 
