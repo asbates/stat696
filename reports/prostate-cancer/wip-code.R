4472137099380cc5051e81db39137173f0efb32a
Andrew Bates <andrewbates73@gmail.com>
2018-11-04 08:53:45 -0800

    first pass at intro. initial section on methods

--- reports/prostate-cancer/wip-code.R
+++ reports/prostate-cancer/wip-code.R
@@ -2,22 +2,75 @@
 library(here)
 library(readr)
 library(dplyr)
+library(forcats)
 
 prostate <- read_tsv(here("data", "prostate.txt"))
 
-# ======================
-# ======= EDA ==========
-# ======================
+# ======================================
+# =============== EDA ==================
+# ======================================
+
+
+# ------ numerical summaries -----------
 
 dim(prostate)
+names(prostate)
+str(prostate)
+
+# remove id, rename
+prostate <- prostate %>%
+  select(-id)
+
+prostate <- prostate %>%
+  rename(
+    penetrate = capsule,
+    dre = dpros,
+    caps = dcaps
+  )
+
+# re-encode variables
+prostate <- prostate %>%
+  mutate(
+    penetrate = as.factor(penetrate),
+    race = as.factor(race),
+    dre = as.factor(dre),
+    caps = as.factor(caps)
+  ) %>%
+  mutate(
+    penetrate = fct_recode(penetrate, yes = "1", no = "0"),
+    race = fct_recode(race, white = "1", black = "2"),
+    caps = fct_recode(caps, no = "1", yes = "2"),
+    dre = fct_recode(dre,
+                     "no nodule" = "1",
+                     "unilobar left" = "2",
+                     "unilobar right" = "3",
+                     "bilobar" = "4")
+  )
+
+str(prostate)
+
 
 # how many have experience capsule penetration?
 prostate %>%
-  group_by(capsule) %>%
+  group_by(penetrate) %>%
   summarise(n = n())
 
 # any missing values?
-apply(prostate, 2, function(x)sum(is.na(x)))
+apply(prostate, 2, function(x) sum(is.na(x)) )
 
+# race has 3 missing values
+summary(prostate)
+
+prostate %>%
+  filter(is.na(race))
+
+# remove missing observations
+prostate <- prostate %>%
+  filter(!is.na(race))
+
+dim(prostate)
+prostate %>%
+  group_by(penetrate) %>%
+  summarise(n = n())
 
 
